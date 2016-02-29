90dba536d127e9303eca71483175789b19acfc4d
mawirep <mawirep@uni.coventry.ac.uk>
2016-02-29 10:57:09 +0000

    Added files via upload

---
+++ VirtualRobot.py
@@ -0,0 +1,64 @@
+# Use the Tkinter library to create GUI
+from Tkinter import *
+#Set up a GUI called window.
+
+
+#What the button does
+def start():
+    pass
+
+
+window = Tk()
+window.title("WorldMap")
+canvas = Canvas(width = 700, height = 350, bg = 'black')
+canvas.pack(expand = YES, fill = BOTH)
+gif1 = PhotoImage(file = 'worldmap3.gif')
+canvas.create_image(80, 20, image = gif1, anchor = NW)
+
+root = Tk()
+frame = Frame(root)
+frame.pack()
+
+startbutton = Button(frame, text="Start", fg="green")
+startbutton.pack(side = TOP )
+quitbutton = Button(frame, text="Quit", fg="red")
+quitbutton.pack(side = BOTTOM )
+item1button = Button(frame, text="Item1", fg="black")
+item1button.pack(side = BOTTOM )
+item2button = Button(frame, text="Item2", fg="black")
+item2button.pack(side = BOTTOM )
+item3button =  Button(frame, text="Item3", fg="black")
+item3button.pack(side = BOTTOM )
+
+
+
+
+
+
+
+
+
+
+mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
